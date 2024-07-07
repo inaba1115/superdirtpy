389aadac52c2a530f9aea1fdd084f4d9be89cbe8
inaba1115 <inaba1115@gmail.com>
2024-07-07 23:53:34 +0900

    add random

--- superdirtpy/chords.py
+++ superdirtpy/chords.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Chords:
     """
     ref: https://github.com/tidalcycles/Tidal/blob/dev/src/Sound/Tidal/Chords.hs
@@ -62,3 +65,65 @@ class Chords:
     minor7sharp5flat9 = [0, 3, 8, 10, 13]
     eleven_sharp = [0, 4, 7, 10, 14, 18]
     minor11sharp = [0, 3, 7, 10, 14, 18]
+
+    @classmethod
+    def random(cls, with_name: bool = True) -> tuple[str, list[int]] | list[int]:
+        chords = [
+            ("major", Chords.major),
+            ("aug", Chords.aug),
+            ("six", Chords.six),
+            ("six_nine", Chords.six_nine),
+            ("major7", Chords.major7),
+            ("major9", Chords.major9),
+            ("add9", Chords.add9),
+            ("major11", Chords.major11),
+            ("add11", Chords.add11),
+            ("major13", Chords.major13),
+            ("add13", Chords.add13),
+            ("dom7", Chords.dom7),
+            ("dom9", Chords.dom9),
+            ("dom11", Chords.dom11),
+            ("dom13", Chords.dom13),
+            ("seven_flat5", Chords.seven_flat5),
+            ("seven_sharp5", Chords.seven_sharp5),
+            ("seven_flat9", Chords.seven_flat9),
+            ("nine", Chords.nine),
+            ("eleven", Chords.eleven),
+            ("thirteen", Chords.thirteen),
+            ("minor", Chords.minor),
+            ("diminished", Chords.diminished),
+            ("minor_sharp5", Chords.minor_sharp5),
+            ("minor6", Chords.minor6),
+            ("minor_six_nine", Chords.minor_six_nine),
+            ("minor7flat5", Chords.minor7flat5),
+            ("minor7", Chords.minor7),
+            ("minor7sharp5", Chords.minor7sharp5),
+            ("minor7flat9", Chords.minor7flat9),
+            ("minor7sharp9", Chords.minor7sharp9),
+            ("diminished7", Chords.diminished7),
+            ("minor9", Chords.minor9),
+            ("minor11", Chords.minor11),
+            ("minor13", Chords.minor13),
+            ("minor_major7", Chords.minor_major7),
+            ("one", Chords.one),
+            ("five", Chords.five),
+            ("sus2", Chords.sus2),
+            ("sus4", Chords.sus4),
+            ("seven_sus2", Chords.seven_sus2),
+            ("seven_sus4", Chords.seven_sus4),
+            ("nine_sus4", Chords.nine_sus4),
+            ("seven_flat10", Chords.seven_flat10),
+            ("nine_sharp5", Chords.nine_sharp5),
+            ("minor9sharp5", Chords.minor9sharp5),
+            ("seven_sharp5flat9", Chords.seven_sharp5flat9),
+            ("minor7sharp5flat9", Chords.minor7sharp5flat9),
+            ("eleven_sharp", Chords.eleven_sharp),
+            ("minor11sharp", Chords.minor11sharp),
+        ]
+
+        chord = random.choice(chords)
+
+        if with_name:
+            return chord
+        else:
+            return chord[1]
