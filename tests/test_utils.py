f9890ebdc92c34348eca264bdf40faf9bfeee9d5
inaba1115 <inaba1115@gmail.com>
2024-07-23 14:24:41 +0900

    fix test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,8 +6,8 @@ from superdirtpy.utils import zmap
 class TestUtils(unittest.TestCase):
     def test_zmap(self):
         self.assertAlmostEqual(zmap(5, 0, 10, 0, 100), 50.0)
-        self.assertEqual(round(zmap(0.5, 0, 1, 0, 127)), 64)
+        self.assertEqual(int(zmap(0.5, 0, 1, 0, 127)), 63)
         self.assertAlmostEqual(round(zmap(64, 0, 127, 0, 1), 1), 0.5)
-        self.assertEqual(round(zmap(0, -1, 1, 0, 127)), 64)
-        self.assertEqual(round(zmap(2, 0, 1, 0, 127)), 127)
-        self.assertEqual(round(zmap(-2, 0, 1, 0, 127)), 0)
+        self.assertEqual(int(zmap(0, -1, 1, 0, 127)), 63)
+        self.assertEqual(int(zmap(2, 0, 1, 0, 127)), 127)
+        self.assertEqual(int(zmap(-2, 0, 1, 0, 127)), 0)
